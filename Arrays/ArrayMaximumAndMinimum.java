8311acc84927a73b5d9fb6adcd305ec3e98b5436
smruti12344 <72293400+smruti12344@users.noreply.github.com>
2024-06-22 09:39:58 +0530

    updated file

--- Arrays/ArrayMaximumAndMinimum.java
+++ Arrays/ArrayMaximumAndMinimum.java
@@ -1,14 +1,25 @@
  package Arrays;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
 public class ArrayMaximumAndMinimum {
     public static int[] findMaxAndMin(int arr[]){
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-       Arrays.sort(arr);
-       max = arr[arr.length-1];
-       min = arr[0];
+        // 1st process of Tc: O(nlogn) and Sc:O(1)
+    //    Arrays.sort(arr);
+    //    max = arr[arr.length-1];
+    //    min = arr[0];
+      
+    // 2nd process of Tc:O(n) and Sc:O(1)
+      for (int i : arr) {
+        if (i>max) {
+            max=i;
+        }
+        if(i<min){
+            min=i;
+        }
+      }
         return new int[]{max,min};
     }
 public static void main(String[] args) {
