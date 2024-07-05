63bfb0dda873c76d63802a28c65ee5dd40f6d1ab
smruti12344 <72293400+smruti12344@users.noreply.github.com>
2024-07-05 20:40:44 +0530

    MaximumSubarray

--- Arrays/MaximumSubarry.java
+++ Arrays/MaximumSubarry.java
@@ -1,9 +1,27 @@
  
 package Arrays;
 public class MaximumSubarry {
-    
+    public int maxiSum(int arr[]){
+        //kadian's algo
+        int maxSum = 0;
+        int currSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(currSum>=0){
+                currSum+=arr[i];
+            }else{
+                currSum=arr[i];
+            }
+            if (currSum>maxSum) {
+                maxSum=currSum;
+            }
+        }
+
+        return maxSum;
+    }
 public static void main(String[] args) {
-    
+    int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+    int result = new MaximumSubarry().maxiSum(arr);
+    System.out.println(result);
 }
     
 }
