053d7c1546960881627efd893302e5b1ebb47388
smruti12344 <72293400+smruti12344@users.noreply.github.com>
2024-07-23 11:31:35 +0530

    Longest Substring Without Repeating Characters

--- String/LongestCommomSubString.java
+++ String/LongestCommomSubString.java
@@ -4,25 +4,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LongestCommomSubString {
-    public int lengthOfLongestSubstring(String s) {
-        int  maxCount=0;
-        int j=0;
-        Set<Character> set = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-           if (set.add(ch)) {
-            maxCount = Math.max(maxCount, i-j+1);
+    
+        public int lengthOfLongestSubstring(String s) {
+            int n = s.length();
+            int maxLength = 0;
+            Set<Character> charSet = new HashSet<>();
+            int left = 0;
             
-           }else{
-            while (s.charAt(j)!=ch) {
-                set.remove(s.charAt(j));
-                j++;
-                
+            for (int right = 0; right < n; right++) {
+                if (!charSet.contains(s.charAt(right))) {
+                    charSet.add(s.charAt(right));
+                    maxLength = Math.max(maxLength, right - left + 1);
+                } else {
+                    while (charSet.contains(s.charAt(right))) {
+                        charSet.remove(s.charAt(left));
+                        left++;
+                    }
+                    charSet.add(s.charAt(right));
+                }
             }
-           }
+            
+            return maxLength;
         }
-        return maxCount;
-    }
+    
 public static void main(String[] args) {
     String str = "abcabcbb";
     System.out.println(new LongestCommomSubString().lengthOfLongestSubstring(str));
